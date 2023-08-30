4bee6b23cb3e32c106e6c0b83921a1bf20daa38b
smilyj <88810147+smilyj@users.noreply.github.com>
2023-08-30 07:23:38 -0400

    Update madlib.py

--- madlib.py
+++ madlib.py
@@ -1,3 +1,4 @@
+
 #user input
 name = input("what is your name?: ")
 time1 = input("Give me a time?: ")
@@ -12,3 +13,5 @@ verb = input("Give me a (Past Tense) verb: ")
 madlib = f"Hi! My name is {name}. I woke up at {time1} to go to school, but school start at {time2}.\ 
  After walking my {pronoun} I decided to go get ready! I changed into my favorite outfit- a dressy pants \
  and a {noun1}. Then it was time to eat my go to breakfast which is a {noun2}. I then {verb} to school."
+
+print(madlib)
